ff23275298a950b97bd96c04c404135859cab23c
Abdul-Qadir3 <aqadirsamdani@gmail.com>
2024-05-02 23:33:03 +0500

    input and argv method for user input

---
+++ 10_loop_input/input.py
@@ -0,0 +1,12 @@
+# name :str = input("Please enter your name : \t")
+
+# print(type(name))
+# print(f"Welecome dear use {name}!")
+
+import sys
+
+print("line1")
+print("line2")
+
+print(type(sys.argv))# check the type of argv
+print(sys.argv) #itrative data type
